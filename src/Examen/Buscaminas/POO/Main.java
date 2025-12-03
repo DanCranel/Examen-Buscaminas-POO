2dc8369907e0e5b81484a0c9f05efb9ea07d5326
GabrielSeZa <gserranoz1@est.ups.edu.ec>
2025-12-03 16:12:09 -0500

    Agregue esto a la clase Main:

--- src/Examen/Buscaminas/POO/Main.java
+++ src/Examen/Buscaminas/POO/Main.java
@@ -1,5 +1,15 @@
 package Examen.Buscaminas.POO;
 
+import Examen.Buscaminas.POO.controlador.ControladorJuego;
+import Examen.Buscaminas.POO.vista.VistaConsola;
+
+
 public class Main {
+	
+	public static void main(String[] args) {
+		VistaConsola vista = new VistaConsola();
+		ControladorJuego controlador = new ControladorJuego(vista);
+		controlador.iniciar();
+	}
 
 }
