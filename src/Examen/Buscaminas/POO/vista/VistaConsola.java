ab2130fc3c1def3051d78bd9663edb2405e8aedb
User <User@LAPTOP-0FDIEI2P>
2025-12-03 19:33:10 -0900

    Agregar clase Main y esqueletos de VistaConsola y ControladorJuego

---
+++ src/Examen/Buscaminas/POO/vista/VistaConsola.java
@@ -0,0 +1,19 @@
+package Examen.buscaminas.POO.vista;
+
+public class VistaConsola {
+
+    public VistaConsola() {
+    }
+
+    public void mostrarPortada() {
+        // implementación vendrá después
+    }
+
+    public String pedirNombreJugador() {
+        return "";
+    }
+
+    public int mostrarMenuPrincipal() {
+        return 0;
+    }
+}
